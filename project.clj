35bec1a77c614d49fc4e0fdcfb7a3bfe52b8606f
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-24 16:22:38 +0100

    Renaming the project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gif-clj "1.0.0-SNAPSHOT"
+(defproject animated-gif-clj "0.1.0-SNAPSHOT"
   :description "a small gif encoder"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :java-source-paths ["src/java"])
