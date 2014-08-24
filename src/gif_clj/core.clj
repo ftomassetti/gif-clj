230ca245741ccdb1c85ea314c7fad3467760191f
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-24 16:16:23 +0100

    Adding function to build gif in memory

--- src/gif_clj/core.clj
+++ src/gif_clj/core.clj
@@ -1,5 +1,7 @@
 (ns gif-clj.core
-  (:import AnimatedGifEncoder [com.github.gif AnimatedGifEncoder]))
+  (:import [com.github.gif AnimatedGifEncoder]))
+
+(import java.io.ByteArrayOutputStream)
 
 (defn write-gif [filename imgs delay loops]
   (let [encoder
@@ -12,6 +14,27 @@
 	  [img imgs]
 	(if img (.addFrame encoder img)))
       (.finish encoder))))
+
+
+(defn build-animated-gif
+  "Build an animated gives and return the corresponding bytes array.
+   Delay:  delay in milliseconds between each frame
+   Repeat: number of times the gif should repeat (0 means forever)
+   Frames: sequence of frames, as BufferedImage"
+  [delay repeat frames]
+  (let [ baos (ByteArrayOutputStream.)
+         encoder (doto (com.github.gif.AnimatedGifEncoder.)
+                   (.start baos)
+                   (.setDelay delay)
+                   (.setRepeat repeat))]
+    (do
+      (doseq [frame frames]
+        (.addFrame encoder frame))
+      (.finish encoder)
+      (.flush baos)
+      (let [bytes (.toByteArray baos)
+            _ (.close baos)]
+        bytes))))
 
 (defn load-image [file]
   (try (javax.imageio.ImageIO/read file)
